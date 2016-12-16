f655f089963d18a00e56a95cd4ace0680ee5ba73
Leonheadt <sacri_@naver.com>
2016-12-16 15:50:41 +0900

    Test_Blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def Get_Test(request):
+
+    receive = request.GET.get('value')
+    print(receive)
+
+    return HttpResponse()
+
+
